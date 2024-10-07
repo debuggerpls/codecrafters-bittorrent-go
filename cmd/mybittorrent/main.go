1ad07bd410fd8e12d1b52d0f124866245acb7fb8
Deividas Puplauskas <ngivingup@gmail.com>
2024-10-07 20:12:58 +0200

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -16,10 +16,11 @@ var _ = json.Marshal
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	firstRune := rune(bencodedString[0])
+func decodeBencodeString(bencodedString string) (string, int, error) {
+	index := 0
+
 	switch {
-	case unicode.IsDigit(firstRune):
+	case unicode.IsDigit(rune(bencodedString[index])):
 		var firstColonIndex int
 
 		for i := 0; i < len(bencodedString); i++ {
@@ -33,19 +34,103 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 
 		length, err := strconv.Atoi(lengthStr)
 		if err != nil {
-			return "", err
+			return "", index, err
 		}
 
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	case firstRune == 'i':
+		index = firstColonIndex + 1 + length
+		decodedString := bencodedString[firstColonIndex+1 : index]
+
+		return decodedString, index, nil
+	default:
+		return "", 0, fmt.Errorf("invalid BencodeString %q", bencodedString)
+	}
+}
+
+func decodeBencodeInteger(bencodedString string) (int, int, error) {
+	index := 0
+
+	switch c := rune(bencodedString[index]); c {
+	case 'i':
 		indexEnd := strings.Index(bencodedString, "e")
 		integer, err := strconv.Atoi(bencodedString[1:indexEnd])
 		if err != nil {
+			return 0, 0, err
+		}
+
+		index = indexEnd + 1
+		return integer, index, nil
+
+	default:
+		return 0, 0, fmt.Errorf("invalid BencodeInteger %q", bencodedString)
+	}
+}
+
+func decodeBencodeList(bencodedString string) ([]interface{}, int, error) {
+	index := 0
+	depth := 0
+	l := make([]interface{}, 0)
+
+	for {
+		switch c := rune(bencodedString[index]); {
+		case c == 'e':
+			return l, index + 1, nil
+		case c == 'l':
+			if depth == 0 {
+				index += 1
+				depth += 1
+			} else {
+				// TODO: refactor to not use recursion
+				// this is a nested list
+				nl, relIndex, err := decodeBencodeList(bencodedString[index:])
+				if err != nil {
+					return nil, index, err
+				}
+				l = append(l, nl)
+				index += relIndex
+			}
+		case c == 'i':
+			i, relIndex, err := decodeBencodeInteger(bencodedString[index:])
+			if err != nil {
+				return nil, index, err
+			}
+			l = append(l, i)
+			index += relIndex
+		case unicode.IsDigit(c):
+			s, relIndex, err := decodeBencodeString(bencodedString[index:])
+			if err != nil {
+				return nil, index, err
+			}
+			l = append(l, s)
+			index += relIndex
+		default:
+			return nil, index, fmt.Errorf("Unknown:\n%s\n", bencodedString[index:])
+		}
+	}
+}
+
+func decodeBencode(bencodedString string) (interface{}, error) {
+	c := rune(bencodedString[0])
+	switch {
+	case unicode.IsDigit(c):
+		result, _, err := decodeBencodeString(bencodedString)
+		if err != nil {
 			return "", err
 		}
-		return integer, nil
+		return result, nil
+	case c == 'i':
+		result, _, err := decodeBencodeInteger(bencodedString)
+		if err != nil {
+			return "", err
+		}
+		return result, nil
+	case c == 'l':
+		result, _, err := decodeBencodeList(bencodedString)
+		if err != nil {
+			return "", err
+		}
+		return result, nil
 	default:
-		return "", fmt.Errorf("Only strings are supported at the moment")
+		return "", fmt.Errorf("Unsupported:\n%s\n", bencodedString)
 	}
 }
 
