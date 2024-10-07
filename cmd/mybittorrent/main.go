16a4e7f120bb292eaf346851d517b9e68d12773f
Deividas Puplauskas <ngivingup@gmail.com>
2024-10-07 20:43:52 +0200

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -103,7 +103,88 @@ func decodeBencodeList(bencodedString string) ([]interface{}, int, error) {
 			l = append(l, s)
 			index += relIndex
 		default:
-			return nil, index, fmt.Errorf("Unknown:\n%s\n", bencodedString[index:])
+			return nil, index, fmt.Errorf("invalid BencodeList %q", bencodedString[index:])
+		}
+	}
+}
+
+func decodeBencodeDict(bencodedString string) (map[string]interface{}, int, error) {
+	index := 0
+	depth := 0
+	isValue := false
+	key := ""
+	d := make(map[string]interface{})
+
+	for {
+		switch c := rune(bencodedString[index]); {
+		case c == 'e':
+			return d, index + 1, nil
+		case c == 'd':
+			switch {
+			case depth == 0:
+				index += 1
+				depth += 1
+			case isValue:
+				// TODO: refactor recursion out
+				nd, relIndex, err := decodeBencodeDict(bencodedString[index:])
+				if err != nil {
+					return nil, index, err
+				}
+				d[key] = nd
+				index += relIndex
+				isValue = false
+			default:
+				return nil, index, fmt.Errorf("invalid BencodeDict %q", bencodedString[index:])
+			}
+		case unicode.IsDigit(c):
+			switch {
+			case depth == 0:
+				return nil, index, fmt.Errorf("invalid BencodeDict %q", bencodedString[index:])
+			case !isValue:
+				s, relIndex, err := decodeBencodeString(bencodedString[index:])
+				if err != nil {
+					return nil, index, err
+				}
+				key = s
+				index += relIndex
+				isValue = true
+			case isValue:
+				s, relIndex, err := decodeBencodeString(bencodedString[index:])
+				if err != nil {
+					return nil, index, err
+				}
+				d[key] = s
+				index += relIndex
+				isValue = false
+			default:
+				return nil, index, fmt.Errorf("invalid BencodeDict, unexpected string %q", bencodedString[index:])
+			}
+		case c == 'i':
+			if isValue {
+				i, relIndex, err := decodeBencodeInteger(bencodedString[index:])
+				if err != nil {
+					return nil, index, err
+				}
+				d[key] = i
+				index += relIndex
+				isValue = false
+			} else {
+				return nil, index, fmt.Errorf("invalid BencodeDict, unexpected integer %q", bencodedString[index:])
+			}
+		case c == 'l':
+			if isValue {
+				l, relIndex, err := decodeBencodeList(bencodedString[index:])
+				if err != nil {
+					return nil, index, err
+				}
+				d[key] = l
+				index += relIndex
+				isValue = false
+			} else {
+				return nil, index, fmt.Errorf("invalid BencodeDict, unexpected list %q", bencodedString[index:])
+			}
+		default:
+			return nil, index, fmt.Errorf("invalid BencodeDict %q", bencodedString[index:])
 		}
 	}
 }
@@ -125,6 +206,12 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 		return result, nil
 	case c == 'l':
 		result, _, err := decodeBencodeList(bencodedString)
+		if err != nil {
+			return "", err
+		}
+		return result, nil
+	case c == 'd':
+		result, _, err := decodeBencodeDict(bencodedString)
 		if err != nil {
 			return "", err
 		}
